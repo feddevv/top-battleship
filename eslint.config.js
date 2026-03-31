127959cd14963c46bd8e924bd80ded2755c8652a
Fedya1221 <fst20071221@gmail.com>
2026-03-31 15:21:54 +0300

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default defineConfig([
+  js.configs.recommended,
+
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      // 'prettier/prettier': 'error',
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+
+  prettierConfig,
+])
