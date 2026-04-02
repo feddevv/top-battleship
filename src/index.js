44ed2ef23e46789f02e19c60acf18e83e312c1e3
Fedya Styopochkin <fst20071221@gmail.com>
2026-04-02 16:39:29 +0000

    feat: implement playRound method with win condition

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { GameController } from './modules/GameController.js'
 
 const game = new GameController()
 game.populatePlayerFleet()
+game.populateComputerFleet()
 
 game.render()
 game.initEventListeners()
--- src/modules/GameController.js
+++ src/modules/GameController.js
@@ -1,6 +1,7 @@
 import { Player } from './Player.js'
 import { DOMController } from './DOMController.js'
 
+// cSpell:ignore gameboard
 export class GameController {
   #player
   #computer
@@ -26,21 +27,93 @@ export class GameController {
   // Temporary func
   populatePlayerFleet() {
     this.#player.gameboard.placeShip([0, 0], 4, 'hor')
-
     this.#player.gameboard.placeShip([2, 0], 3, 'vert')
     this.#player.gameboard.placeShip([2, 2], 3, 'hor')
     this.#player.gameboard.placeShip([2, 0], 3, 'vert')
-
     this.#player.gameboard.placeShip([4, 2], 2, 'hor')
     this.#player.gameboard.placeShip([6, 0], 2, 'vert')
     this.#player.gameboard.placeShip([2, 7], 2, 'hor')
-
     this.#player.gameboard.placeShip([9, 0], 1, 'hor')
     this.#player.gameboard.placeShip([9, 9], 1, 'hor')
     this.#player.gameboard.placeShip([0, 9], 1, 'hor')
     this.#player.gameboard.placeShip([5, 8], 1, 'hor')
+  }
 
-    console.log(this.#player.gameboard.gameboard)
+  populateComputerFleet() {
+    this.#computer.gameboard.placeShip([0, 0], 4, 'hor')
+
+    this.#computer.gameboard.placeShip([2, 0], 3, 'vert')
+    this.#computer.gameboard.placeShip([2, 2], 3, 'hor')
+    this.#computer.gameboard.placeShip([2, 0], 3, 'vert')
+
+    this.#computer.gameboard.placeShip([4, 2], 2, 'hor')
+    this.#computer.gameboard.placeShip([6, 0], 2, 'vert')
+    this.#computer.gameboard.placeShip([2, 7], 2, 'hor')
+
+    this.#computer.gameboard.placeShip([9, 0], 1, 'hor')
+    this.#computer.gameboard.placeShip([9, 9], 1, 'hor')
+    this.#computer.gameboard.placeShip([0, 9], 1, 'hor')
+    this.#computer.gameboard.placeShip([5, 8], 1, 'hor')
+  }
+
+  playRound(x, y) {
+    if (this.#turn === this.#computer) return
+
+    if (this.playerMove(x, y)) return
+
+    this.#turn = this.#computer
+
+    setTimeout(() => this.computerMove(), 600)
+  }
+
+  playerMove(x, y) {
+    const isHit = this.#computer.gameboard.receiveAttack([x, y])
+    this.render()
+
+    if (this.#computer.gameboard.isLost()) {
+      alert('Player won!')
+      return true
+    }
+
+    if (isHit) {
+      return true
+    } else {
+      this.#turn = this.#computer
+    }
+  }
+
+  computerMove() {
+    const coord = this.makeRandomMove()
+    const isHit = this.#player.gameboard.receiveAttack(coord)
+
+    this.render()
+
+    if (this.#player.gameboard.isLost()) {
+      alert('Computer won!')
+      return
+    }
+
+    if (isHit) {
+      setTimeout(() => {
+        this.computerMove()
+      }, 600)
+    } else {
+      this.#turn = this.#player
+    }
+  }
+
+  makeRandomMove() {
+    const playerGameboard = this.#player.gameboard
+    let x, y
+    do {
+      x = Math.floor(Math.random() * 9)
+      y = Math.floor(Math.random() * 9)
+    } while (
+      playerGameboard.gameboard[x][y] === 'miss' ||
+      playerGameboard.gameboard[x][y] === 'hit'
+    )
+
+    return [x, y]
   }
 
   initEventListeners() {
@@ -58,9 +131,7 @@ export class GameController {
 
         const x = e.target.dataset.x
         const y = e.target.dataset.y
-        this.#computer.gameboard.receiveAttack([x, y])
-
-        this.render()
+        this.playRound(x, y)
       }
     })
   }
